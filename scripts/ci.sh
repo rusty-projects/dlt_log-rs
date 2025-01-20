af3d163edfdd9d9de9c9734cea9ae0b3998975ac
Martin Kunkel <ich@martinkunkel.de>
2025-01-20 21:33:34 +0000

    Refactor CI workflow and improve code coverage checks
    Removed LCOV setup from CI workflow and updated coverage command
    to enforce a minimum line coverage of 80%. Enhanced output
    grouping for better readability during CI execution.

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -2,19 +2,29 @@
 set -eu
 export CARGO_TERM_COLOR=always
 
+echo "::group::building"
 cargo build
+echo "::endgroup::"
 
 # linting
+echo "::group::linting"
 cargo clippy -- -D warnings
 cargo fmt -- --check
+echo "::endgroup::"
 
 # test without running dlt-daemon
+echo "::group::test without running dlt-daemon"
 killall --wait dlt-daemon || true
 cargo test --test init_ok
+echo "::endgroup::"
 
 # test with running dlt-daemon
+echo "::group::test with running dlt-daemon"
 dlt-daemon -d
 cargo test
+echo "::endgroup::"
 
 # code coverage
-cargo llvm-cov --lcov --output-path lcov.info --remap-path-prefix
+echo "::group::code coverage, required: 80% line coverage"
+cargo llvm-cov --fail-under-lines 80
+echo "::endgroup::"
