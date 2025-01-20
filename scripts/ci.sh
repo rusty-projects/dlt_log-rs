ac7fbe549754bfcf74ba8887463b622a0813efc3
Martin Kunkel <ich@martinkunkel.de>
2025-01-20 19:59:36 +0000

    Add code coverage reporting to CI workflow
    - Integrate coverage reporting using zgosalvez/github-actions-report-lcov
    - Set minimum coverage threshold to 90%
    - Update CI script to include linting and specific test execution

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 set -eu
+export CARGO_TERM_COLOR=always
 
 cargo build
-
-# test without running dlt-daemon
-killall --wait dlt-daemon || true
-cargo test
-
-# test with running dlt-daemon
-dlt-daemon -d
-cargo test
 
 # linting
 cargo clippy -- -D warnings
 cargo fmt -- --check
+
+# test without running dlt-daemon
+killall --wait dlt-daemon || true
+cargo test --test init_ok
+
+# test with running dlt-daemon
+dlt-daemon -d
+cargo test
 
 # code coverage
 cargo llvm-cov
