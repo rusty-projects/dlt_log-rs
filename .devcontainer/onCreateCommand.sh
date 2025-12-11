470d86fb2f58e9c9a6101deb705ec0b3d944e5a5
Martin Kunkel <ich@martinkunkel.de>
2025-12-11 16:58:37 +0000

    Add pre-commit tool

--- .devcontainer/onCreateCommand.sh
+++ .devcontainer/onCreateCommand.sh
@@ -21,3 +21,8 @@ fi
 # for coverage measurement (install after setting up the correct toolchain)
 rustup component add llvm-tools
 curl -LsSf https://github.com/taiki-e/cargo-llvm-cov/releases/download/v0.6.21/cargo-llvm-cov-x86_64-unknown-linux-gnu.tar.gz | tar xzf - -C /usr/local/cargo/bin
+
+# pre-commit
+sudo apt-get install -y python3-pip
+pip3 install --break-system-packages pre-commit
+pre-commit install
